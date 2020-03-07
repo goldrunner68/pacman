1557423512c679ac3ba3198e45236bc3abc6a260
goldrunner68 <pilouweb2000@gmail>
2020-03-07 21:18:53 +0100

    creation du plateau avec des rectangles

--- src/view/ViewJeu.java
+++ src/view/ViewJeu.java
@@ -2,8 +2,16 @@ package view;
 
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Screen;
+
+import java.util.ArrayList;
+
+import static javafx.scene.paint.Color.RED;
 
 public class ViewJeu {
     private Group root;
@@ -12,6 +20,11 @@ public class ViewJeu {
     private Jeu jeu;
     private ImageView imageDeFond;
     private double vitesse = 10;
+    Text affichagePlateau;
+    private ArrayList niveau1;
+    private Text aff;
+    private Rectangle rectangle;
+
     //
 
 
@@ -22,38 +35,63 @@ public class ViewJeu {
         this.viewHandler = viewHandler;
 
         initBackground();
+
         afficherJeu();
     }
+private void univers(){
+    int plateau[] ={
+            1,1,1,1,
+            2,2,2,1,
+    };
+
+    niveau1 = new ArrayList();
+
+    for(int i = 0; i < 8; i++)
+    {
+        niveau1.add(plateau[i]);
+
+
+        if (niveau1.get(0).equals(1)){
+            Rectangle rectangle = new Rectangle();
+            rectangle.setX(20);
+            rectangle.setY(50);
+            rectangle.setWidth(200);
+            rectangle.setHeight(150);
+            rectangle.setFill(Color.RED);
+        }
+        if (niveau1.get(0).equals(2)){
+            Rectangle rectangle = new Rectangle();
+            rectangle.setX(20);
+            rectangle.setY(50);
+            rectangle.setWidth(200);
+            rectangle.setHeight(150);
+            rectangle.setFill(Color.BLUE);
+        }
+    }
+
+}
     private void initBackground() {
-        TiledMap tiledMap = new TmxMapLoader().load("level1.tmx");
-        TiledMapRenderer tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-        OrthographicCamera camera = new OrthographicCamera(960, 900);
-        camera.position.set(posX, posY, 0);
-        camera.update();
-        tiledMapRenderer.setView(camera);
-        tiledMapRenderer.render();
+
+
+
         imageDeFond = new ImageView("Asset/Images/pacmanFondInGame.jpg");
         Rectangle2D primaryScreenBounds = Screen.getPrimary().getBounds();
         imageDeFond.setFitHeight((int) primaryScreenBounds.getHeight());
         imageDeFond.setFitWidth((int) primaryScreenBounds.getWidth());
+
+
     }
 
     public void afficherJeu() {
         root.getChildren().clear();
-        root.getChildren().addAll(imageDeFond);
+        root.getChildren().add(rectangle);
 
         }
 
 
 
 
-    public void moveBareLeft() {
-        spritePacman.setX(spritePacman.getX() - vitesse);
-    }
 
-    public void moveBareRight() {
-        spritePacman.setX(spritePacman.getX() + vitesse);
-    }
 
 
 
