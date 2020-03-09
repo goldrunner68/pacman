cd88b2a07c54205361edb19594a0e71017290460
goldrunner68 <pilouweb2000@gmail.com>
2020-03-09 19:56:00 +0100

    code degeu Mvc mais ca marche

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,4 +1,68 @@
 package model;
 
+import javafx.scene.Group;
+import javafx.scene.image.ImageView;
+
+import java.util.ArrayList;
+
 public class Model {
+    private Group root;
+
+    private ImageView boule,brique;
+    private ArrayList niveau1;
+
+    public Model(Group root, ImageView boule, ArrayList niveau1) {
+        this.root = root;
+        this.boule = boule;
+        this.niveau1 = niveau1;
+    }
+
+
+    private int[] level(){
+        int plateau[] = {
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
+                1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 3,
+                1, 1, 2, 1, 2, 2, 2, 1, 2, 1, 3,
+                0, 2, 2, 2, 2, 2, 2, 1, 2, 0, 3,//44
+                1, 2, 1, 2, 2, 2, 2, 2, 2, 1, 3,
+                1, 2, 1, 1, 2, 2, 1, 2, 1, 1, 3,
+                1, 2, 2, 2, 2, 1, 1, 2, 1, 1, 3,
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3//44
+        };
+
+        return plateau;
+    }
+    public void constructionLevel(){
+        int[] plateau = level();
+        niveau1 = new ArrayList();
+        int col = 0;
+        int ligne =0;
+        for(int index = 0; index < 88; index++)
+        {
+            col = col+64;
+            niveau1.add(plateau[index]);
+            if (niveau1.get(index).equals(3)){// si 3 passe a la ligne
+                ligne = ligne+64;
+                col =0;
+            }
+            if (niveau1.get(index).equals(1)){// si 1 affiche une brique
+                brique = new ImageView("Asset/Images/Brick_Block.png");
+                brique.setX(600+col);
+                brique.setY(300+ligne);
+                brique.setFitWidth(64);
+                brique.setFitHeight(64);
+                root.getChildren().add(brique);
+            }
+            if (niveau1.get(index).equals(2)){// si 1 affiche une boule
+                boule = new ImageView("Asset/Images/boule.png");
+                boule.setX(600+col);
+                boule.setY(300+ligne);
+                boule.setFitWidth(64);
+                boule.setFitHeight(64);
+                root.getChildren().add(boule);
+            }
+
+
+        }
+    }
 }
--- src/view/ViewJeu.java
+++ src/view/ViewJeu.java
@@ -2,10 +2,9 @@ package view;
 
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Screen;
+import model.Model;
 
 import java.util.ArrayList;
 
@@ -14,6 +13,7 @@ public class ViewJeu {
     private Jeu jeu;
     private ImageView imageDeFond,boule,brique;
     private ArrayList niveau1;
+    private Model univers;
 
 
     //
@@ -27,51 +27,9 @@ public class ViewJeu {
         initBackground();
         afficherJeu();
     }
-private void univers(){
-    int plateau[] ={
-            1,1,1,1,1,1,1,1,1,1,3,
-            1,2,2,1,2,2,2,2,2,1,3,
-            1,1,2,1,2,2,2,1,2,1,3,
-            0,2,2,2,2,2,2,1,2,0,3,//44
-            1,2,1,2,2,2,2,2,2,1,3,
-            1,2,1,1,2,2,1,2,1,1,3,
-            1,2,2,2,2,1,1,2,1,1,3,
-            1,1,1,1,1,1,1,1,1,1,3//44
-    };
-    niveau1 = new ArrayList();
-    int col = 0;
-    int ligne =0;
-    for(int index = 0; index < 88; index++)
-    {
-        col = col+64;
-        niveau1.add(plateau[index]);
-        if (niveau1.get(index).equals(3)){// si 3 passe a la ligne
-           ligne = ligne+64;
-           col =0;
-        }
-        if (niveau1.get(index).equals(1)){// si 1 affiche une brique
-            brique = new ImageView("Asset/Images/Brick_Block.png");
-            brique.setX(600+col);
-            brique.setY(300+ligne);
-            brique.setFitWidth(64);
-            brique.setFitHeight(64);
-            root.getChildren().add(brique);
-        }
-        if (niveau1.get(index).equals(2)){// si 1 affiche une boule
-            boule = new ImageView("Asset/Images/boule.png");
-            boule.setX(600+col);
-            boule.setY(300+ligne);
-            boule.setFitWidth(64);
-            boule.setFitHeight(64);
-            root.getChildren().add(boule);
-        }
 
-
-    }
-
-}
     private void initBackground() {
-
+        univers = new Model(root,imageDeFond,niveau1);
         imageDeFond = new ImageView("Asset/Images/pacmanFondInGame.jpg");
         Rectangle2D primaryScreenBounds = Screen.getPrimary().getBounds();
         imageDeFond.setFitHeight((int) primaryScreenBounds.getHeight());
@@ -82,7 +40,9 @@ private void univers(){
 
     public void afficherJeu() {
         root.getChildren().clear();
-        univers();
+        root.getChildren().add(imageDeFond);
+        univers.constructionLevel();
+
 
         }
 
