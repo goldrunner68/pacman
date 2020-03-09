a4e2758ced30a2427c8f6b2f69ec3074ff88a325
goldrunner68 <pilouweb2000@gmail.com>
2020-03-09 20:28:19 +0100

    Mvc modulo model level construction

--- src/model/Model.java
+++ src/model/Model.java
@@ -20,14 +20,14 @@ public class Model {
 
     private int[] level(){
         int plateau[] = {
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3,
-                1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 3,
-                1, 1, 2, 1, 2, 2, 2, 1, 2, 1, 3,
-                0, 2, 2, 2, 2, 2, 2, 1, 2, 0, 3,//44
-                1, 2, 1, 2, 2, 2, 2, 2, 2, 1, 3,
-                1, 2, 1, 1, 2, 2, 1, 2, 1, 1, 3,
-                1, 2, 2, 2, 2, 1, 1, 2, 1, 1, 3,
-                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3//44
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                1, 2, 2, 1, 2, 2, 2, 2, 2, 1,
+                1, 1, 2, 1, 2, 2, 2, 1, 2, 1,
+                0, 2, 2, 2, 2, 2, 2, 1, 2, 0,
+                1, 2, 1, 2, 2, 2, 2, 2, 2, 1,
+                1, 2, 1, 1, 2, 2, 1, 2, 1, 1,
+                1, 2, 2, 2, 2, 1, 1, 2, 1, 1,
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1
         };
 
         return plateau;
@@ -37,17 +37,17 @@ public class Model {
         niveau1 = new ArrayList();
         int col = 0;
         int ligne =0;
-        for(int index = 0; index < 88; index++)
+        for(int index = 0; index < level().length; index++)
         {
             col = col+64;
             niveau1.add(plateau[index]);
-            if (niveau1.get(index).equals(3)){// si 3 passe a la ligne
+            if ((index%10)==0){// si 3 passe a la ligne
                 ligne = ligne+64;
                 col =0;
             }
             if (niveau1.get(index).equals(1)){// si 1 affiche une brique
                 brique = new ImageView("Asset/Images/Brick_Block.png");
-                brique.setX(600+col);
+                brique.setX(670+col);
                 brique.setY(300+ligne);
                 brique.setFitWidth(64);
                 brique.setFitHeight(64);
@@ -55,7 +55,7 @@ public class Model {
             }
             if (niveau1.get(index).equals(2)){// si 1 affiche une boule
                 boule = new ImageView("Asset/Images/boule.png");
-                boule.setX(600+col);
+                boule.setX(670+col);
                 boule.setY(300+ligne);
                 boule.setFitWidth(64);
                 boule.setFitHeight(64);
